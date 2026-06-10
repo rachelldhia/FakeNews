ced23c6677fcfd65b7798b1a60d3c5550c5c9dfb
Racheldhia <dhiarachella@gmail.com>
2026-06-11 02:36:37 +0700

    update konfigurasi url api

--- frontend/app.js
+++ frontend/app.js
@@ -127,10 +127,9 @@ document.addEventListener('DOMContentLoaded', () => {
             // 3. Make HTTP request (local server for localhost, production server for Vercel)
             const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
             
-            // PERBAIKAN: Ditambahkan '/predict' di ujung URL Railway
             const apiUrl = isLocal 
                 ? 'http://localhost:5001/predict' 
-                : 'fakenewsml-production-9bb0.up.railway.app'; 
+                : 'https://fakenewsml-production.up.railway.app/predict';
 
             const response = await fetch(apiUrl, {
                 method: 'POST',
